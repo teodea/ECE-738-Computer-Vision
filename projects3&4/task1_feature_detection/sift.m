c4e409182f10f8a9489565f01db83f1c1005f04d
Carter Karl Falkenberg <73714985+CarterKFalkenberg@users.noreply.github.com>
2025-02-27 12:58:32 -0500

    Example usage of sift on an image

---
+++ projects3&4/task1_feature_detection/sift.m
@@ -0,0 +1,8 @@
+path = 'ReconstructionData/L_rectified_stereoU1.jpg';
+I_RGB = imread(path);
+% sift only works on grayscale, but can still use x,y coords 
+I_GRAY = rgb2gray(I_RGB);
+features = detectSIFTFeatures(I_GRAY);
+imshow(I_RGB);
+hold on;
+plot(features.selectStrongest(100))
